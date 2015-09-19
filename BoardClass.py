7ae56babb8561ce65f3fb3aa40d464d0cdddfb6b
Brian Kirby <bkirby57@gmail.com>
2015-09-19 15:36:24 -0400

    added function to find average of surrounding items in board

--- BoardClass.py
+++ BoardClass.py
@@ -26,3 +26,27 @@ class GameBoard(object):
         frac = num / den
         output = input + (scale * frac) + 1
         return output
+
+    # this function averages the item at the target_index with the 8
+    # surrounding items. assumes square matrices only, assumes one
+    # end of the board connects to other
+    def eight_surrounding(self, input_matrix, target_index):
+        dimension = input_matrix.shape[0]
+        target_item = input_matrix.item(target_index)
+        x = target_index[0]
+        y = target_index[1]
+        xp = np.mod(target_index[0] + 1, dimension)
+        xm = np.mod(target_index[0] - 1, dimension)
+        yp = np.mod(target_index[1] + 1, dimension)
+        ym = np.mod(target_index[1] - 1, dimension)
+        west = input_matrix.item(x, ym)
+        northwest = input_matrix.item(xm, ym)
+        north = input_matrix.item(xm, y)
+        northeast = input_matrix.item(xm, yp)
+        east = input_matrix.item(x, yp)
+        southeast = input_matrix.item(xp, yp)
+        south = input_matrix.item(xp, y)
+        southwest = input_matrix.item(xp, ym)
+        all_direction = [target_item, west, northwest, north, northeast,
+                         east, southeast, south, southwest]
+        return np.average(all_direction)
