b2f0f51bdf1ed5e2b3cad28fad280ca269613e52
Brian Kirby <bkirby57@gmail.com>
2015-09-19 13:34:16 -0400

    added create_Board function to BoardClass

--- BoardClass.py
+++ BoardClass.py
@@ -8,12 +8,13 @@ class GameBoard(object):
     def __init__(self, initial_size):
 
         # board will be initial_Size x initial_Size
-        self.initial_Size = initial_size
+        self.initial_size = initial_size
 
         # create initial board
-        self.initial_Board = np.ones((initial_size, initial_size))
+        self.initial_Board = np.ones((self.initial_size, self.initial_size))
 
     # creates a random setup for board initially
-    def create_Board(self, weight):
-    	
-
+    def create_board(self, weight):
+        r_matrix = np.random.rand(self.initial_size, self.initial_size)
+        r_matrix_weighted = weight * r_matrix
+        self.board = self.initial_Board + r_matrix_weighted
