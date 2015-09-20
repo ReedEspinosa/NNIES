c93da3f152463c1f5cee79ea08457c2d62d621f3
Reed Espinosa <reedespinosa@umbc.edu>
2015-09-19 21:38:55 -0400

    animals class tested and debugged

--- classTester.py
+++ classTester.py
@@ -4,5 +4,9 @@ import numpy as np
 beast = Animal([0, 0], 10, 1, 2, 20)
 
 surroundings = np.zeros(18)
-surroundings[1] = 1;
-print beast.act(surroundings)
+surroundings[1] = 1
+print "-----------"
+print "deltaLives: %d" %(beast.act(surroundings, [100, 100]))
+print "new location: (%d,%d)" %(beast.cords[0], beast.cords[1])
+print "new life: %f" %(beast.life)
+print "-----------"
