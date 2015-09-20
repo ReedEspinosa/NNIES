35bae19ca02b4a191a1f88bce3cc9637be1366bb
Brian Kirby <bkirby57@gmail.com>
2015-09-20 12:01:56 -0400

    added function for finding surrounding plant life

--- overallGameClass.py
+++ overallGameClass.py
@@ -13,16 +13,25 @@ class OverallGame(object):
         self.land_map = GameBoard(board_size)
 
         # create initial list of herbivores
-        self.animal_list = [Animal([np.random.randint(0, board_size),
-                            np.random.randint(0, board_size)])
-                            for count in xrange(herb_number)]
+        self.herb_list = [Animal([np.random.randint(0, board_size),
+                          np.random.randint(0, board_size)])
+                          for count in xrange(herb_number)]
 
-    # returns a list of the positions of all animals
-    def animal_position(self):
-        return [self.animal_list[i].cords
-                for i in xrange(len(self.animal_list))]
+    # returns a list of the positions of all herbivores
+    def herb_position_list(self):
+        return [self.herb_list[i].cords
+                for i in xrange(len(self.herb_list))]
 
+    # for a given animal this creates a list of surroundings
+    def surrounding_list(self, herb_obj):
+        position = (herb_obj.cords[0], herb_obj.cords[1])
+        land = self.land_map.board
+        return self.land_map.eight_surrounding_list(land, position)
+
+# TEST:
 # game = OverallGame(10, 5)
 # print game.land_map.board
-# print game.animal_list[0].cords
-# print game.animal_position()
+# print game.herb_list[0].cords
+# print game.herb_list[1].cords
+# print game.herb_position_list()
+# print game.surrounding_list(game.herb_list[1])
